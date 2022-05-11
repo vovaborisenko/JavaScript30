7af7a4201793731329dc1c3e00fc90a7cc35b342
Uladzimir Barysenka <uladzimir.barysenka@innowise-group.com>
2022-05-11 19:39:00 +0300

    adds array methods

---
+++ 07 - Array Cardio Day 2/main.js
@@ -0,0 +1,35 @@
+"use strict";
+(function () {
+    const people = [
+        { name: "Wes", year: 1988 },
+        { name: "Kait", year: 1986 },
+        { name: "Irv", year: 1970 },
+        { name: "Lux", year: 2015 },
+    ];
+    const comments = [
+        { text: "Love this!", id: 523423 },
+        { text: "Super good", id: 823423 },
+        { text: "You are the best", id: 2039842 },
+        { text: "Ramen is my fav food ever", id: 123523 },
+        { text: "Nice Nice Nice!", id: 542328 },
+    ];
+    // Some and Every Checks
+    // Array.prototype.some() // is at least one person 19 or older?
+    const isAdult = people.some((person) => new Date().getFullYear() - person.year >= 19);
+    // Array.prototype.every() // is everyone 19 or older?
+    const isAdultAll = people.every((person) => new Date().getFullYear() - person.year >= 19);
+    console.table({ isAdult, isAdultAll });
+    // Array.prototype.find()
+    // Find is like filter, but instead returns just the one you are looking for
+    // find the comment with the ID of 823423
+    const commentId = 823423;
+    const comment = comments.find(({ id }) => id === commentId);
+    console.table(comment);
+    // Array.prototype.findIndex()
+    // Find the comment with this ID
+    // delete the comment with the ID of 823423
+    const index = comments.findIndex(({ id }) => id === commentId);
+    console.table(comments);
+    console.table([...comments.slice(0, index), ...comments.slice(index + 1)]);
+    console.table(comments.filter(({ id }) => id !== commentId));
+})();
